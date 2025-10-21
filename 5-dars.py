bf25ea41de4dd7457eef9556ce013e2a2a8394b1
20100105 <shomurotovamashhura272@gmail.com>
2025-10-21 18:22:39 +0300

    first commit

---
+++ 5-dars.py
@@ -0,0 +1,80 @@
+# string , methods, f"" string
+
+#ism ='bobur'
+#familya ='murodov'
+#otchestva ='anvarovich'
+#FIO = f"Assalomu alaykum {familya} {ism} {otchestva}!"
+
+#print(FIO)
+#print(f"Assalomu alaykum {familya.title()} {ism.title()} {otchestva.title()}!")
+
+#capitalize -matndagi kichik hafrlarni katta qiladi
+#meva ='anor, olma, anjir'
+#txt = f'bobur hello'
+#x =txt.capitaliza()
+#print(x)
+
+# lover(),caserfold matndagi barcha katta hafrlarni kichik qiladi
+#ism'Ibodullayev Ahmedov'
+#txt = f'Bobur hello'
+#x =txt.casefold()
+#print(x)
+#print(ism.lover())
+
+#center-malumotni o'rtasiga bo'sh joy qo'shdi
+#txt='banan'
+#x=txt.center(12)
+#print(x)
+
+#count-string malumotda nechta ma'lum bir so'z yoki harf borligini hisoblaydi
+#txt="I love apples,apple are my favorite fruit"
+#x=txt.count("p")
+#print(x)
+
+#endswith()string malumot ma'lum bir so'z yoki harf bilan tugashini tekshiradi
+#txt="Hello, welcome to my world"
+#x=txt.endswith("")
+#print(x)
+
+#expandtabs() String malumotdagi tab belgilarni bosh joy bilan almashtiradi
+#txt="H\te\ti\tl\to"
+#ism='K\tamolb\tek'
+#x=ism.expandtabs("2")
+#print(ism)
+#print(x) 
+
+#find() string malumotda ma'lum bir so'z yoki harfning indekisini qaytaradi
+#txt="Hello, welcome to my world"
+#x=txt.find("welcome")
+#print(x)
+
+
+# ism= input ("Ismingizni kiriting :")
+# yosh=int(input("Yoshingizni kiriting:"))
+# manzil=input("Manzilingizni kiriting:")
+# maktab=input("Maktabingizni kiriting:")
+# qiziqish=input("Qiziqishingizni kiriting:")
+# jura=input("Yaqin insoningizni kiriting: ")
+
+# print(f"Hurmatli {ism.title()} ning yoshi {yosh} da, \n {manzil.title()}da joylashgan \n {maktab} maktabda o'qiydi. \n Uning qiziqishlari {qiziqish.title()} dan iborat ")
+
+#upper()string malumotda ketma-ketligni katta harfga aylantiradi
+# txt ="Hello my friends"
+# x =txt.upper()
+# print(x)
+
+#zfill() string malumotda ketma-ketni 0 qiymatlari bilan to'ldiradi
+# txt="50"
+# x =txt.zfill(10)
+# print(x)
+
+#strip() string malumotda ketma-ketlikning qisqartirilgan versiyani qaytaradi
+# txt="  banana , strowberry  "
+# x=txt.strip()
+# print("of all fruits", x,"is my favorite")
+
+#lower() string malumotda ketma-ketlikni kichik harfga aylantiradi
+# txt="Hello my FRIENDS"
+# x=txt.lower()
+# print(x)
+
