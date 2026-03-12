82027e4f650299a6b5731711173cac18c9552d62
Shomurotova Mashhura <shomurotovamashhura04@gmail.com>
2026-03-12 17:09:07 +0500

    ..
    ..

---
+++ 10-dars.py
@@ -0,0 +1,8 @@
+# a = int(input("Uch xonali son kiriting:"))
+# birlik = a % 10
+# onlik = (a//110)%10
+# yuzlik =a//100
+# print("siz kiritgan son:",a)
+# print("birlar xonasidagi son:",birlik)
+# print("onlar xonasidagi son:",onlik)
+# print("birlik*100+onlik*10+yuzlik")
