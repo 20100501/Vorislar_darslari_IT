025e8a08cb4d0fdbadd234916e922e73987b1b77
Shomurotova Mashhura <shomurotovamashhura04@gmail.com>
2026-03-12 17:04:55 +0500

    zz
    zz

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+# ismlar = ['Ilyosbek,''mardanboy','karimboy','murodbek']
+# print(type(ismlar))
+
+#   thisdict = {
+#     "brand": "Ford",
+#     "model": "Mustang",
+#     "year":  1964
+# }
+# print(thisdict)
+
+# mashhura = {
+#     'ism': 'mashhura',
+#     'yosh':15,
+#     'familya': 'shomurotova',
+#     'manzil' : 'qoraqosh' ,
+#     'maktab' : '1-sonli maktab' ,
+#     'sinf' : 9,
+#     'qiziqish': 'kitob oqish',
+#     'transbort': 'BMV' ,
+#     'oqish' : 'orta'
+# }
+
+# print(f"{mashhura['ism'].title()} {mashhura['familya'].title()} ")
+
